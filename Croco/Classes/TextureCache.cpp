861bad4304f65d765c92d34d6b1c41ad91418be6
totrin <totrin@MacBook-Air-Alex.local>
2012-12-04 16:04:07 +0400

    Revert "Revert "tmp2""
    This reverts commit dd88c91d63dc7cc8104f1d2da98657d3f0e42f11.

--- Croco/Classes/TextureCache.cpp
+++ Croco/Classes/TextureCache.cpp
@@ -12,7 +12,7 @@
 using namespace cocos2d;
 
 TextureCacheLayer::TextureCacheLayer()
-: m_nNumberOfSprites(75)
+: m_nNumberOfSprites(71)
 , m_nNumberOfLoadedSprites(0)
 {
 	CCSize size = CCDirector::sharedDirector()->getWinSize();
@@ -57,14 +57,9 @@ TextureCacheLayer::TextureCacheLayer()
     ADDIMAGE(Timer_timer_digits_light)
     ADDIMAGE(Timer_timer_digits_passive)
     ADDIMAGE(Timer_timer_forest)
-    //Desk 3
+    //Desk 2
     ADDIMAGE(_desk)
     ADDIMAGE(_c_big)
-    ADDIMAGE(_c_big_all)
-    //Activity 3
-    ADDIMAGE(act_o)
-    ADDIMAGE(act_p)
-    ADDIMAGE(act_r)
     //GameMenu 11
     ADDIMAGE(GameMenu_gamemenu)
     ADDIMAGE(GameMenu_help)
